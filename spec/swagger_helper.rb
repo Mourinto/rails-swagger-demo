1def78179ca0cfacf969ae791140103b04d024d7
Mourinto <monier@mailbox.org>
2020-07-27 11:19:17 +0200

    cahnge from yaml to json

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -15,29 +15,44 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
-    'v1/swagger.yaml' => {
-      openapi: '3.0.1',
-      info: {
-        title: 'API V1',
-        version: 'v1'
-      },
-      paths: {},
-      servers: [
-        {
-          url: 'https://{defaultHost}',
-          variables: {
-            defaultHost: {
-              default: 'www.example.com'
-            }
-          }
-        }
-      ]
-    }
+      'v1/swagger.json' => {
+          openapi: '3.0.1',
+          info: {
+              title: 'API V1',
+              version: 'v1'
+          },
+          definitions: {
+              errors_object: {
+                  type: 'object',
+                  properties: {
+                      errors: {'$ref' => '#/definitions/errors_map'}
+                  }
+              },
+              errors_map: {
+                  type: 'object',
+                  additionalProperties: {
+                      type: 'array',
+                      items: {type: 'string'}
+                  }
+              }
+          },
+          paths: {},
+          servers: [
+              {
+                  url: 'https://{defaultHost}',
+                  variables: {
+                      defaultHost: {
+                          default: 'localhost:3000'
+                      }
+                  }
+              }
+          ]
+      }
   }
 
   # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
   # The swagger_docs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.swagger_format = :yaml
+  config.swagger_format = :json
 end
